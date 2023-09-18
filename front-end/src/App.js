88ef2e45e0195cc0606cbd523c531bc6ccfe85ec
louis <kwezi.louishg@gmail.com>
2023-09-18 15:06:43 +0300

    adding a form and alert

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,34 @@
-import {Link, Outlet} from 'react-router-dom';
-
+import { useState } from 'react';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
+import Alert from './components/Alert';
 
 function App() {
+  const [jwtToken, setJwtToken] = useState("");
+  const [alertMessage, setAlertMessage] = useState("");
+  const [alertClassName, setAlertClassName] = useState("d-none");
+
+  const navigate = useNavigate();
+
+  const logOut = () => {
+    setJwtToken("");
+    navigate("/login");
+  }
+
   return (
     <div className="container">
       <div className="row">
-        <div className="col>">
+        <div className="col">
           <h1 className="mt-3">Watch a Movie!</h1>
         </div>
         <div className="col text-end">
-          <Link to="/login"><span className="badge bg-success">Login</span></Link>
+          {jwtToken === ""
+            ? <Link to="/login" className="text-decoration-none"><span className="badge bg-success" >Login</span></Link>
+            : <a href="#!" onClick={logOut}><span className='badge bg-danger'>Logout</span></a>
+          }
         </div>
         <hr className="mb-3"></hr>
       </div>
+
       <div className="row">
         <div className="col-md-2">
           <nav>
@@ -20,17 +36,26 @@ function App() {
               <Link to="/" className="list-group-item list-group-item-action">Home</Link>
               <Link to="/movies" className="list-group-item list-group-item-action">Movies</Link>
               <Link to="/genres" className="list-group-item list-group-item-action">Genres</Link>
-              <Link to="/admin/movie/0" className="list-group-item list-group-item-action">Add Movie</Link>
-              <Link to="/manage-catalogue" className="list-group-item list-group-item-action">Manage Catalogue</Link>
-              <Link to="/graphql" className="list-group-item list-group-item-action">GraphQl</Link>
-
+              {jwtToken !== "" &&
+                <>
+                  <Link to="/admin/movie/0" className="list-group-item list-group-item-action">Add Movie</Link>
+                  <Link to="/manage-catalogue" className="list-group-item list-group-item-action">Manage Catalogue</Link>
+                  <Link to="/graphql" className="list-group-item list-group-item-action">GraphQl</Link>
+                </>
+              }
             </div>
           </nav>
         </div>
         <div className="col-md-10">
-        <Outlet />
+          <Alert
+            message={alertMessage}
+            className={alertClassName}
+          />
+          <Outlet context={{
+            jwtToken, setJwtToken,setAlertClassName,setAlertMessage,
+          }} />
         </div>
-       
+
       </div>
     </div>
   );
--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -1,11 +1,55 @@
+import { useState } from "react";
+import Input from "./form/input";
+import { useOutletContext } from "react-router-dom";
 
 const Login =() => {
+    const [email,setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const {setJwtToken} =useOutletContext();
+    const {setAlertClassName} =useOutletContext();
+    const {setAlertMessage} =useOutletContext();
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("email/pass",email,password);
+
+        if (email === "admin@example.com"){
+          setJwtToken("abc");
+          setAlertClassName("d-none");
+          setAlertMessage("");
+        }else {
+            setAlertClassName("alert-danger");
+            setAlertMessage("Invalid credentials");
+        }
+    }
     return(
         
-        <div>
+        <div className="col-md-6 offset-md-3">
             <h2>Login</h2>
             <hr />
-            
+            <form onSubmit={handleSubmit}>
+                <Input
+                    title="Email Address"
+                    type="email"
+                    className="form-control"
+                    name="email"
+                    autoComplete="email-new"
+                    onChange={(event) => setEmail(event.target.value)}
+                />
+                 <Input
+                    title="Password"
+                    type="password"
+                    className="form-control"
+                    name="password"
+                    autoComplete="password-new"
+                    onChange={(event) => setPassword(event.target.value)}
+                />
+                <hr/>
+                <Input 
+                type="submit"
+                className="btn btn-primary"
+                value="Login"
+                /> 
+            </form>
         </div>
         
     )
