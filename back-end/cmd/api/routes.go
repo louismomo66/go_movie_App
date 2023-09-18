edad7902c15a089e63bae37b007631bdf0f7355a
louis <kwezi.louishg@gmail.com>
2023-09-18 16:33:57 +0300

    adding a list of movies

--- back-end/cmd/api/routes.go
+++ back-end/cmd/api/routes.go
@@ -11,5 +11,6 @@ func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
 	mux.Use(middleware.Recoverer)
 	mux.Get("/", app.Home)
+	mux.Get("/movies", app.AllMovies)
 	return mux
 }
