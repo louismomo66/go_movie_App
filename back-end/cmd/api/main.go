e383d06d13d893b5378ce07321ad7c33aef81e14
louis <kwezi.louishg@gmail.com>
2023-09-19 14:22:48 +0300

    adding database connection

--- back-end/cmd/api/main.go
+++ back-end/cmd/api/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"backend/internal/models/repository"
+	"backend/internal/models/repository/dbrepo"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,21 +12,29 @@ import (
 const port = 9000
 
 type application struct {
+	DSN    string
 	Domain string
+	DB     repository.DatabaseRepo
 }
 
 func main() {
 	//set application config
 	var app application
 	//read from command line
-
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgress connection string")
+	flag.Parse()
 	//connect to the database
-
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	app.DB = &dbrepo.PostgresDBRepo{DB: conn}
+	defer app.DB.Connection().Close()
 	//start a web server
 	app.Domain = "example.com"
 	log.Println("Starting application on port", port)
 
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
